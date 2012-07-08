4ecdf71e982735e88b55fee15be1c15a3e9f8336
Razvan Ranca <ranca.razvan@gmail.com>
2012-07-08 14:55:17 +0100

    Added backgrounds

--- Main.hs
+++ Main.hs
@@ -18,12 +18,12 @@ getPoints :: Display -> [Point]
 getPoints (a,b) = [((a-k),(b-q),0)| q<-[1 .. a], k<-[1 .. b]]
 
 main :: IO ()
-main  = do r1 <- readFile "mandelbrotSC.ppm" --"checkerboard.ppm"
-           r2 <- readFile "mandelbrotSC.ppm" --"alpforward.ppm"
-           r3 <- readFile "mandelbrotSC.ppm" --"alpup.ppm"           
-           r4 <- readFile "mandelbrotSC.ppm" --"checkerboard.ppm"
-           r5 <- readFile "mandelbrotSC.ppm" --"alprightC.ppm"
-           r6 <- readFile "mandelbrotSC.ppm" --"checkerboard.ppm"     
+main  = do r1 <- readFile "backgrounds/mandelbrotSC.ppm" --"checkerboard.ppm"
+           r2 <- readFile "backgrounds/mandelbrotSC.ppm" --"alpforward.ppm"
+           r3 <- readFile "backgrounds/mandelbrotSC.ppm" --"alpup.ppm"           
+           r4 <- readFile "backgrounds/mandelbrotSC.ppm" --"checkerboard.ppm"
+           r5 <- readFile "backgrounds/mandelbrotSC.ppm" --"alprightC.ppm"
+           r6 <- readFile "backgrounds/mandelbrotSC.ppm" --"checkerboard.ppm"     
            let
                r11 = read ("[" ++ replace r1 "\n" "," ++ "0]") :: [Int]
                r22 = read ("[" ++ replace r2 "\n" "," ++ "0]") :: [Int]
